c932473ff89e6fa9e12d637c221a39133e77bd1a
proSamael <pro100.ink@gmail.com>
2023-08-31 12:14:39 +0300

    update

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
 namespace CE102R51;
 
 use Exception;
+use M236\Mercury;
 
 require 'vendor/autoload.php';
 
 try {
     $en = new Energomer('127.0.0.1', 5010, 98765);
+    $me = new Mercury('127.0.0.1',5010, 98765);
     echo '<pre>';
     print_r($en->ReadDaysEnergy()->get());
 
